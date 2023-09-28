cfd4ecd80b60080653928e8ee998e157401a7ff1
Nick Desaulniers <ndesaulniers@google.com>
2023-09-27 18:10:45 -0700

    [MachineInstr] add insert method for variadic instructions
    As alluded to in #20571, it would be nice if we could mutate operand
    lists of MachineInstr's more safely.

--- llvm/include/llvm/CodeGen/MachineInstr.h
+++ llvm/include/llvm/CodeGen/MachineInstr.h
@@ -1812,6 +1812,8 @@ public:
   /// preferred.
   void addOperand(const MachineOperand &Op);
 
+  void insert(mop_iterator It, ArrayRef<MachineOperand> Ops);
+
   /// Replace the instruction descriptor (thus opcode) of
   /// the current instruction with a new one.
   void setDesc(const MCInstrDesc &TID) { MCID = &TID; }
